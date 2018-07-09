1896346be195a1d9ad15bcc0f1279328a08a435c
linwang <lin.wang@cybex.io>
2018-07-09 15:07:28 +0800

    add superTextView控件

--- app/src/main/java/com/cybex/gma/client/ui/main/fragment/WalletFragment.java
+++ app/src/main/java/com/cybex/gma/client/ui/main/fragment/WalletFragment.java
@@ -8,12 +8,11 @@ import com.cybex.gma.client.ui.main.presenter.WalletPresenter;
 import com.hxlx.core.lib.mvp.lite.XFragment;
 
 /**
- *
  * 钱包Fragment
  *
  * Created by wanglin on 2018/7/9.
  */
-public class WalletFragment extends XFragment<WalletPresenter>{
+public class WalletFragment extends XFragment<WalletPresenter> {
 
     public static WalletFragment newInstance() {
         Bundle args = new Bundle();
@@ -31,6 +30,7 @@ public class WalletFragment extends XFragment<WalletPresenter>{
     public void initData(Bundle savedInstanceState) {
 
     }
+
 
     @Override
     public int getLayoutId() {
---
+++ app/src/main/java/com/cybex/gma/client/utils/SizeUtil.java
@@ -0,0 +1,43 @@
+package com.cybex.gma.client.utils;
+
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+/**
+ * 屏幕尺寸相关方法
+ */
+public class SizeUtil {
+
+    public static DisplayMetrics getDisplayMetrics() {
+        return Resources.getSystem().getDisplayMetrics();
+    }
+
+    public static int getScreenWidth() {
+        return getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        return getDisplayMetrics().heightPixels;
+    }
+
+    public static int px2dp(float pxValue) {
+        final float scale = getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int dp2px(float dpValue) {
+        final float scale = getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2sp(float pxValue) {
+        final float fontScale = getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int sp2px(float spValue) {
+        final float fontScale = getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+}
