3d82ad870f36209f426c9a9f9ab44cf63807fc44
linwang <lin.wang@cybex.io>
2018-07-12 21:59:25 +0800

    移除model

--- app/src/main/java/com/cybex/gma/client/ui/model/AccountInfo.java
+++
@@ -1,7 +0,0 @@
-package com.cybex.gma.client.ui.model;
-
-/**
- * Created by wanglin on 2018/7/11.
- */
-public class AccountInfo {
-}
--- app/src/main/java/com/cybex/gma/client/ui/model/TransferRecord.java
+++
@@ -1,9 +0,0 @@
-package com.cybex.gma.client.ui.model;
-
-/**
- * 交易记录对象
- *
- * Created by wanglin on 2018/7/12.
- */
-public class TransferRecord {
-}
