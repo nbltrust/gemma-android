c26baf29b371e98a8314308ea632d1cb23dd3428
linwang <lin.wang@cybex.io>
2018-07-12 22:12:48 +0800

    update JNIUtil

--- app/src/main/java/com/cybex/gma/client/ui/JNIUtil.java
+++ app/src/main/java/com/cybex/gma/client/ui/JNIUtil.java
@@ -1,6 +1,7 @@
 package com.cybex.gma.client.ui;
 
-import java.util.Map;
+import android.support.v4.util.Pair;
+
 
 /**
  * Jni调用
@@ -16,9 +17,29 @@ public class JNIUtil {
     private JNIUtil() {}
 
 
-    public final static native Map<String, String> createKey();
+    /**
+     * 获取公私钥对
+     *
+     * @return
+     */
+    public final static native Pair<String, String> createKey();
 
+
+    /**
+     * 加密私钥获取密文
+     *
+     * @param password
+     * @param priv_key
+     * @return
+     */
     public final static native String get_cypher(String password, String priv_key);
 
+    /**
+     * 根据密文和密码获取私钥
+     *
+     * @param cipher_keys
+     * @param password
+     * @return
+     */
     public final static native String get_private_key(String cipher_keys, String password);
 }
