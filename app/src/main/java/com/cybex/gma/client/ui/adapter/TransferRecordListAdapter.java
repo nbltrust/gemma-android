63de0c16f00215178436a2d8c5bb63660f75fb0d
linwang <lin.wang@cybex.io>
2018-07-12 10:46:58 +0800

    交易记录列表界面

---
+++ app/src/main/java/com/cybex/gma/client/ui/adapter/TransferRecordListAdapter.java
@@ -0,0 +1,27 @@
+package com.cybex.gma.client.ui.adapter;
+
+import android.support.annotation.Nullable;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.cybex.gma.client.R;
+import com.cybex.gma.client.ui.model.TransferRecord;
+
+import java.util.List;
+
+/**
+ * 交易记录Adapter
+ *
+ * Created by wanglin on 2018/7/12.
+ */
+public class TransferRecordListAdapter extends BaseQuickAdapter<TransferRecord, BaseViewHolder> {
+
+    public TransferRecordListAdapter(@Nullable List<TransferRecord> data) {
+        super(R.layout.item_transfer_record_list, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, TransferRecord item) {
+
+    }
+}
