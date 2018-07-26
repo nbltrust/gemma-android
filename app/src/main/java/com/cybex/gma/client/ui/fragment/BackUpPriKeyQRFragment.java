7f2e2107443458031a9e35994248026a46280426
Yiran <ice.blue.zyr@gmail.com>
2018-07-26 16:46:13 +0800

    添加二维码模块相关逻辑

--- app/src/main/java/com/cybex/gma/client/ui/fragment/BackUpPriKeyQRFragment.java
+++ app/src/main/java/com/cybex/gma/client/ui/fragment/BackUpPriKeyQRFragment.java
@@ -2,20 +2,35 @@ package com.cybex.gma.client.ui.fragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cybex.gma.client.R;
+import com.cybex.qrcode.zxing.QRCodeEncoder;
+import com.hxlx.core.lib.common.async.TaskManager;
 import com.hxlx.core.lib.mvp.lite.XFragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
  * 通过二维码备份私钥界面
- *
  */
 public class BackUpPriKeyQRFragment extends XFragment {
 
+    String priKey;
     Unbinder unbinder;
+    @BindView(R.id.iv_fake_QR) ImageView ivFakeQR;
+    @BindView(R.id.iv_real_QR) ImageView ivRealQR;
+    @BindView(R.id.bt_show_QR) Button btShowQR;
+
+    @OnClick(R.id.bt_show_QR)
+    public void showQR(){
+
+    }
+
     public static BackUpPriKeyQRFragment newInstance() {
         Bundle args = new Bundle();
         BackUpPriKeyQRFragment fragment = new BackUpPriKeyQRFragment();
@@ -58,5 +73,18 @@ public class BackUpPriKeyQRFragment extends XFragment {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    public void showRealQR(String privateKey){
+        ivFakeQR.setVisibility(View.GONE);
+        ivRealQR.setVisibility(View.VISIBLE);
+        TaskManager.runOnUIThread(new Runnable() {
+            @Override
+            public void run() {
+                ivRealQR.setImageBitmap(QRCodeEncoder.syncEncodeQRCode(privateKey, 100));
+
+            }
+        });
+
     }
 }
