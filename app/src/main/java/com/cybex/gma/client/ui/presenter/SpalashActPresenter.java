01f1234908068ae1655e941d85fcfa4d799dc6e1
linwang <lin.wang@cybex.io>
2018-08-20 11:47:43 +0800

    修改节点路径

--- app/src/main/java/com/cybex/gma/client/ui/presenter/SpalashActPresenter.java
+++ app/src/main/java/com/cybex/gma/client/ui/presenter/SpalashActPresenter.java
@@ -1,11 +1,16 @@
 package com.cybex.gma.client.ui.presenter;
 
+import android.os.Bundle;
+
 import com.cybex.gma.client.R;
+import com.cybex.gma.client.config.CacheConstants;
+import com.cybex.gma.client.config.ParamConstants;
 import com.cybex.gma.client.db.dao.WalletEntityDao;
 import com.cybex.gma.client.db.entity.WalletEntity;
 import com.cybex.gma.client.manager.DBManager;
 import com.cybex.gma.client.manager.UISkipMananger;
 import com.cybex.gma.client.ui.activity.SpalashActivity;
+import com.cybex.gma.client.utils.SPUtils;
 import com.hxlx.core.lib.mvp.lite.XPresenter;
 import com.hxlx.core.lib.utils.EmptyUtils;
 
@@ -21,6 +26,13 @@ public class SpalashActPresenter extends XPresenter<SpalashActivity> {
         WalletEntityDao dao = DBManager.getInstance().getWalletEntityDao();
         List<WalletEntity> entityList = dao.getWalletEntityList();
         if (EmptyUtils.isNotEmpty(entityList)) {
+            boolean isOpenGesture = SPUtils.getInstance().getBoolean(CacheConstants.KEY_OPEN_GESTURE);
+            if (isOpenGesture) {
+                Bundle bd = new Bundle();
+                bd.putInt(ParamConstants.GESTURE_SKIP_TYPE, ParamConstants.GESTURE_SKIP_TYPE_LOGIN_VERIFY);
+                UISkipMananger.launchVerifyGestureActivity(getV(), bd);
+            }
+
             //跳转到主界面
             UISkipMananger.launchHome(getV());
         } else {
