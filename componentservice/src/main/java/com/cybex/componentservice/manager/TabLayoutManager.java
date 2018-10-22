28b09c26df7cf68d7428ec9b7c5fd8609a083efa
zichen.cai <q615115160@163.com>
2018-10-22 10:14:20 +0800

    init导入页面

--- componentservice/src/main/java/com/cybex/componentservice/manager/TabLayoutManager.java
+++ componentservice/src/main/java/com/cybex/componentservice/manager/TabLayoutManager.java
@@ -1,4 +1,4 @@
-package com.cybex.gma.client.manager;
+package com.cybex.componentservice.manager;
 
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
---
+++ walletmanagement/src/main/java/com/cybex/walletmanagement/ui/activity/ImportWalletGuideActivity.java
@@ -0,0 +1,61 @@
+package com.cybex.walletmanagement.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cybex.componentservice.config.BaseConst;
+import com.cybex.walletmanagement.R;
+import com.hxlx.core.lib.mvp.lite.XActivity;
+
+import me.framework.fragmentation.anim.DefaultHorizontalAnimator;
+import me.framework.fragmentation.anim.FragmentAnimator;
+
+public class ImportWalletGuideActivity extends XActivity {
+
+
+    private ImageView ivDotOne;
+    private TextView tvLookAroundHint;
+    private Button btStartImport;
+
+
+
+    @Override
+    public void bindUI(View view) {
+        ivDotOne = (ImageView) findViewById(R.id.iv_dot_one);
+        tvLookAroundHint = (TextView) findViewById(R.id.tv_look_around_hint);
+        btStartImport = (Button) findViewById(R.id.bt_start_import);
+        btStartImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ImportWalletGuideActivity.this, MnemonicShowActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        setNavibarTitle(getResources().getString(R.string.walletmanage_backup_title), true);
+    }
+
+    @Override
+    public void initData(Bundle savedInstanceState) {
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.walletmanage_activity_import_wallet_guide;
+    }
+
+    @Override
+    public Object newP() {
+        return null;
+    }
+
+    @Override
+    public FragmentAnimator onCreateFragmentAnimator() {
+        // 设置横向(和安卓4.x动画相同)
+        return new DefaultHorizontalAnimator();
+    }
+}
